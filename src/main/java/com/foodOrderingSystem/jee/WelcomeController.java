c78acaea75c78c2d4bf9bed2ef1101b65d75dd0b
Uzval-Mallepeddi <uzval007@gmail.com>
2020-02-09 13:01:11 -0500

    Food Ordering System

---
+++ src/main/java/com/foodOrderingSystem/jee/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.foodOrderingSystem.jee;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class WelcomeController {
+	
+	@RequestMapping(value="/welcome")
+	public String welcome() {
+		return "welcome";
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/views/welcome.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>Food Ordering System</title>
+</head>
+<body>
+Welcome
+</body>
+</html>
