db04b15290114d5d365e27fb2909d046ed367676
Uzval Mallepeddi <uzval007@gmail.com>
2020-02-24 17:30:47 -0500

    Revert "Login & Logout Functionality"

--- src/main/java/com/foodOrderingSystem/config/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package com.foodOrderingSystem.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-	
-	
-}
